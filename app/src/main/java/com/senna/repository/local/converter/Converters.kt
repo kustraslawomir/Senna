c18a317682c900c99d1545e3d2ffe1020d4a87ef
skustra <kustraslawomir@gmail.com>
2019-03-06 19:02:26 +0100

    type converted for List<String>

---
+++ app/src/main/java/com/senna/repository/local/converter/Converters.kt
@@ -0,0 +1,17 @@
+package com.senna.repository.local.converter
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class Converters{
+    @TypeConverter
+    fun fromString(value: String): List<String> {
+        val listType = object : TypeToken<List<String>>() {
+        }.type
+        return Gson().fromJson<List<String>>(value, listType)
+    }
+
+    @TypeConverter
+    fun fromArrayLisr(list: List<String>)= Gson().toJson(list)
+}
