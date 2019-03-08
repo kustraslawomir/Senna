60c1109d945bf515ab860b4ebe460561902dab49
slawomir.kustra <=>
2019-03-08 13:59:06 +0100

    navigate to player fragment

--- app/src/main/java/com/senna/view/activity/nagivation/NavigationActivity.kt
+++ app/src/main/java/com/senna/view/activity/nagivation/NavigationActivity.kt
@@ -2,9 +2,11 @@ package com.senna.view.activity.nagivation
 
 import android.os.Bundle
 import com.senna.com.R
+import com.senna.utils.extensions.addFragment
 import com.senna.utils.extensions.replaceFragment
 import com.senna.view.activity.BaseActivity
 import com.senna.view.fragment.compositions.CompositionsFragment
+import com.senna.view.fragment.player.PlayerFragment
 
 class NavigationActivity : BaseActivity() {
 
@@ -12,5 +14,9 @@ class NavigationActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         replaceFragment(CompositionsFragment(), R.id.content)
+    }
+
+    fun navigateTo(fragment: PlayerFragment) {
+        addFragment(fragment, R.id.content)
     }
 }
