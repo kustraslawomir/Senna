af2917d7cbc76cf604fdd66c0b0965657bbde56a
slawomir.kustra <=>
2019-03-08 14:00:26 +0100

    nullable compositions

--- app/src/main/java/com/senna/repository/local/dao/CompositionDao.kt
+++ app/src/main/java/com/senna/repository/local/dao/CompositionDao.kt
@@ -2,17 +2,15 @@ package com.senna.repository.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.Query
-
-import com.senna.model.databse.Composition
-
 import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import com.senna.model.databse.Composition
 
 @Dao
 interface CompositionDao {
 
     @get:Query("SELECT * FROM composition")
-    val compositions: List<Composition>
+    val compositions: List<Composition>?
 
     @Insert(onConflict = REPLACE)
     fun insertComposition(composition: Composition)
