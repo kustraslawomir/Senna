715ea9c98b3c6084ff545eac1c195de0e6036e6f
skustra <kustraslawomir@gmail.com>
2019-03-24 19:08:13 +0100

    rename method in type converter

--- app/src/main/java/com/senna/repository/local/converter/Converters.kt
+++ app/src/main/java/com/senna/repository/local/converter/Converters.kt
@@ -4,7 +4,7 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-class Converters{
+class Converters {
     @TypeConverter
     fun fromString(value: String): List<String> {
         val listType = object : TypeToken<List<String>>() {
@@ -13,5 +13,5 @@ class Converters{
     }
 
     @TypeConverter
-    fun fromArrayLisr(list: List<String>)= Gson().toJson(list)
+    fun fromArrayList(list: List<String>) : String = Gson().toJson(list)
 }
