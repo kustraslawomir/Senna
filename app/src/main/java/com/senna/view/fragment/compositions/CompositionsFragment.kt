78621cf3ff8827975024bb2a69930220876c5563
slawomir.kustra <kustraslawomir@gmail.com>
2019-03-25 12:45:16 +0100

    lifecycle bug fix

--- app/src/main/java/com/senna/view/fragment/compositions/CompositionsFragment.kt
+++ app/src/main/java/com/senna/view/fragment/compositions/CompositionsFragment.kt
@@ -22,10 +22,7 @@ class CompositionsFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         container.setPaddingFromNavBar(resources)
-    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         val viewModel = getViewModel()
 
         val adapter = CompositionsAdapter(::chooseComposition)
